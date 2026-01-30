74ada40b197554fdfc0cc977eb9f4e77b343267f
Dusan Navidu <navidu200210@gmail.com>
2026-01-30 23:18:51 +0530

    refactor: clean up comments and improve code readability in RootLayout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,25 +6,18 @@ import { LoaderProvider } from "@/context/LoaderContext"
 import { AuthProvider } from "@/context/AuthContext"
 import Toast from 'react-native-toast-message'
 
-// Like App.tsx
 const RootLayout = () => {
   const insets = useSafeAreaInsets()
-  // / device safe area values (top, left, right, and bottom)
   console.log(insets)
   return (
     <LoaderProvider>
       <AuthProvider>
         <View className="flex-1" style={{ marginTop: insets.top }}>
-          {/* Slot renders the currently active screen */}
           <Slot />
         </View>
         <Toast />
       </AuthProvider>
     </LoaderProvider>
-    // <SafeAreaView className="flex-1">
-    // {/* Slot renders the currently active screen */}
-    // <Slot />
-    // </SafeAreaView>
   )
 }
 
